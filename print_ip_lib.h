fc4a560f4bb27f73949b123094a3bd82a51660bf
vabon <bucks7abu@gmail.com>
2018-03-01 20:21:39 +0300

    Remove _t
    Remove _t

--- print_ip_lib.h
+++ print_ip_lib.h
@@ -4,10 +4,10 @@
 #include <type_traits>
 
 template <typename T>
-void print_ip(const std::enable_if_t<std::is_integral<T>::value, T> &);
+void print_ip(const std::enable_if<std::is_integral<T>::value, T> &);
 
 template<typename T>
-void print_ip(const std::enable_if_t<std::is_compound<T>::value, T> &);
+void print_ip(const std::enable_if<std::is_compound<T>::value, T> &);
 
 template <>
 void print_ip<std::string>(const std::string &);
